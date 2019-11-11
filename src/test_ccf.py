5ff5150bd8b87d1007f68d31d105160eb36c1827
orrg <zzx@bigtree.xyz>
2019-11-11 14:21:26 +0800

    update compile

--- src/test_ccf.py
+++ src/test_ccf.py
@@ -7,8 +7,8 @@ import libccf
 
 
 def main():
-    # spec = specio.Spectrum('data/spec-4961-55719-0378.fits')
-    spec = specio.Spectrum('/home/zzx/workspace/data/xiamen/P200-Hale_spec/spec2/fawftbred0027.fits')
+    spec = specio.Spectrum('data/spec-4961-55719-0378.fits')
+    # spec = specio.Spectrum('/home/zzx/workspace/data/xiamen/P200-Hale_spec/spec2/fawftbred0027.fits')
 
     shift = np.arange(-300, 300, 1.0)
     print(len(shift))
