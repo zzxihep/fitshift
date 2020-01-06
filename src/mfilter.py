b0d1bdfc00683c053736680a3a991e4a030c76a9
orrg <zzx@bigtree.xyz>
2020-01-06 13:41:08 +0800

    add a gaussian_filter

--- src/mfilter.py
+++ src/mfilter.py
@@ -1,5 +1,13 @@
+import numpy as np
 import convol
+
+c = 299792.458
 
 
 def gaussian_filter(wave, flux, fwhm=None, R=None):
-    sigma = fwhm / 2.355
+    if fwhm is not None:
+        sigma = fwhm / 2.355
+        k0 = sigma / c
+    else:
+        k0 = 1. / (2.355*R)
+    return np.array(convol.gauss_filter(wave, flux, [0.0, k0]))
